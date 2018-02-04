3ab4672d13b095ea0caa29210fa6d8907be2c74c
Jeremy Cole <jeremy@jcole.us>
2018-02-03 21:43:16 -0800

    Improve examples

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,6 +1,13 @@
+/*
+Read the temperature from an LM75-derived temperature sensor, and display it
+in Celcius every 250ms. Any LM75-derived temperature should work.
+*/
+
 #include <Temperature_LM75_Derived.h>
 
-TI_TMP102 temperature;
+// The Generic_LM75 class will provide 9-bit (±0.5°C) temperature for any
+// LM75-derived sensor. More specific classes may provide better resolution.
+Generic_LM75 temperature;
 
 void setup() {
   while(!Serial) {}
@@ -11,8 +18,9 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("t = ");
-  Serial.println(temperature.readTemperatureF());
+  Serial.print("Temperature = ");
+  Serial.print(temperature.readTemperatureC());
+  Serial.println(" C");
 
   delay(250);
 }
